2e2fc5579cefe138db5dc962e2e731d69474a8d1
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2022-12-30 20:19:38 -0500

    Add game code

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
 console.log('hello world');
+
+let computerSelection;
+let playerSelection;
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    let computerChoice;
+
+    if(randomNum === 1){
+        computerChoice = "rock";
+    } else if(randomNum === 2){
+        computerChoice = "paper";
+    } else{
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+function getUserChoice(){
+    let playerChoice;
+
+    while(true){
+        playerChoice = prompt('Enter "Rock", "Paper", or "Scissors": ');
+        if(playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors'){
+            break;
+        } else if (playerChoice === null){
+            playerChoice = 'no response';
+            break;
+        }
+        else{
+            alert('Enter a valid response');
+            continue;
+        }
+    }
+
+    return playerChoice.toLowerCase();
+}
+
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.substring(1);
+}
+
+function playRound(computerSelection, playerSelection){
+    if(computerSelection === playerSelection){
+        return 'It was a tie';
+    } else if(computerSelection === 'rock' && playerSelection === 'paper'){
+        return 'Paper beats Rock. You win!';
+    } else if(computerSelection === 'paper' && playerSelection === 'scissors'){
+        return 'Scissors beats Paper. You win!';
+    } else if(computerSelection === 'scissors' && playerSelection === 'rock'){
+        return 'Rock beats Scissors. You win!';
+    } else{
+        return `${capitalize(computerSelection)} beats ${capitalize(playerSelection)}. You lose!`
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        computerSelection = getComputerChoice();
+        playerSelection = getUserChoice();
+
+        if(playerSelection !== 'no response'){
+            console.log(playRound(computerSelection, playerSelection));
+        } else {
+            console.log('User declined to play');
+            break;
+        }
+    }
+    console.log('The game has ended');
+}
+
+game();
+
